2ce71e619c23951e3fa1a4af2180565e2907f6b9
Pringlez <john.walsh16609@gmail.com>
2022-07-24 21:59:24 +0100

    Removed manual webpack setup & opted for reacts default packaging Refactored pom setup & added comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './js/main/App';
+import reportWebVitals from './js/main/reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/js/main/App.js
+++ frontend/src/js/main/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './css/App.css';
+import logo from '../../images/logo.svg';
+import '../../css/App.css';
 
 function App() {
   return (
