f56458975b4b5fc08824b1300a47ce39f90fe359
Pringlez <john.walsh16609@gmail.com>
2023-07-03 00:09:10 +0100

    Disabling csrf on h2 console & service APIs Exported latest postman collection

--- service/src/main/java/org/accmanager/service/config/security/SecurityConfig.java
+++ service/src/main/java/org/accmanager/service/config/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin()
                 .and().httpBasic()
                 .and().cors()
-                .and().csrf().disable();
+                .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**");
         http.headers().frameOptions().sameOrigin();
     }
 
